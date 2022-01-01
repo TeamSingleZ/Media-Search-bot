3d1ebab2f46849f962a220cac6fbabb288219746
TeamSingleZ <95397366+TeamSingleZ@users.noreply.github.com>
2022-01-01 16:33:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 default_start_msg = """
 **Hi, I'm Media Search bot**
 
-Here you can search files in inline mode. Just press following buttons and start searching.
+Here you can search files in inline mode. Just press following buttons and start searching.This is the inline mode of AnyStudent Bot.
 """
 
 START_MSG = environ.get('START_MSG', default_start_msg)
